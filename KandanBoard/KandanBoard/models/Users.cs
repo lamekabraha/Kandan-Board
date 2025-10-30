35ef34130135a5db5f08ec5aefafe127209f0d55
lamekabraha <lamekabraha@gmail.com>
2025-10-30 01:52:21 +0000

    Register - HashPassword complete

--- KandanBoard/KandanBoard/models/Users.cs
+++ KandanBoard/KandanBoard/models/Users.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Text.Json;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using System.Threading.Tasks;
 
 
@@ -12,15 +14,15 @@ public class Users
 	public String FirstName { get; set; }
 	public String LastName { get; set; }
 	public String Email { get; set; }
-	public String Password { get; set; }
+	public String HashedPassword { get; set; }
 
-	public Users(int userid, String firstname, String lastname, String email, String password)
+	public Users(int userid, String firstname, String lastname, String email, String hashedpassword)
 	{
 		this.UserId = userid;
 		this.FirstName = firstname;
 		this.LastName = lastname;
 		this.Email = email;
-		this.Password = password;
+		this.HashedPassword = hashedpassword;
 	}
 
 	public Users()
@@ -31,8 +33,8 @@ public class Users
 	public bool EmailExists(string Email)
 	{
 		Console.WriteLine($"Checking if {Email} exists");
-        // used https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/deserialization to learn how to deserialise json file into a list
-        string jsonPath = File.ReadAllText("Data/users.json");
+		// used https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/deserialization to learn how to deserialise json file into a list
+		string jsonPath = File.ReadAllText("Data/users.json");
 		List<Users>? userList = JsonSerializer.Deserialize<List<Users>>(jsonPath);
 		IEnumerable<string> emails = userList.Select(u => u.Email);
 		foreach (string email in emails)
@@ -42,25 +44,8 @@ public class Users
 				Console.WriteLine($"ERROR: {email} already exists.");
 				return true;
 			}
-        }
+		}
 		return false;
-
-		//if (userList != null)
-		//{
-		//	Users? searchEmail = userList.FirstOrDefault(user => user.Email == Email);
-		//	while (true)
-		//	{
-		//		if (searchEmail != null)
-		//		{
-		//			Console.WriteLine($"{Email} aleady exists");
-		//			break;
-		//		}
-		//	}
-		//}
-		//else
-		//{
-		//	Console.WriteLine($"File not found: {userList} does not exist");
-		//}
 	}
 
 	public void Register()
@@ -73,10 +58,35 @@ public class Users
 			user.Email = Console.ReadLine().ToLower();
 			if (EmailExists(user.Email) == false)
 			{
+	
 				break;
 			}
 		}
 		Console.WriteLine("First Name: ");
 		user.FirstName = Console.ReadLine().ToLower();
-    }
+		Console.WriteLine("Last Name: ");
+		user.LastName = Console.ReadLine().ToLower();
+		Console.WriteLine(user.LastName);
+		Console.WriteLine("Password: ");
+		string unhashedPassword = Console.ReadLine();
+
+		user.HashedPassword = HashPassword(unhashedPassword);
+		Console.WriteLine(user.HashedPassword);
+	}
+
+    // using https://learn.microsoft.com/en-us/aspnet/core/security/data-protection/consumer-apis/password-hashing?view=aspnetcore-9.0
+    public string HashPassword(string unhashedPassword)
+	{
+		byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+		Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+
+		string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+			password: unhashedPassword!,
+			salt: salt,
+			prf: KeyDerivationPrf.HMACSHA256,
+			iterationCount: 100000,
+			numBytesRequested: 256 / 8));
+
+		return hashed;
+	}
 }
--- KandanBoard/KandanBoard/obj/Debug/net8.0/KandanBoard.AssemblyInfo.cs
+++ KandanBoard/KandanBoard/obj/Debug/net8.0/KandanBoard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KandanBoard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c801ea070b99239f56f53558d35954127a56fd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb4e867409272247f6a079a028e4fae8639ee9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("KandanBoard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KandanBoard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
