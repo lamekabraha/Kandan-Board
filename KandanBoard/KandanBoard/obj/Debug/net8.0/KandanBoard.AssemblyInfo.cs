1fa1a754f17b14fd92e7d314fb52b559f601be47
lamekabraha <lamekabraha@gmail.com>
2026-01-07 03:08:34 +0000

    created types

--- KandanBoard/KandanBoard/obj/Debug/net8.0/KandanBoard.AssemblyInfo.cs
+++ KandanBoard/KandanBoard/obj/Debug/net8.0/KandanBoard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KandanBoard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c8bedb05fd8c1e9a58913aed9b74bdf9d53eda7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d80ba1fd6f56b7bc7cad5e5c252aba8dc24c67d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("KandanBoard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KandanBoard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
